bdaf56641c1a4733e81a4c4025f16e260e148156
greengbcs2 <ulasmak-green@yahoo.com>
2015-12-08 17:30:35 +0200

    Question View uploaded.

---
+++ app/src/main/java/android/atilim/tr/questionsbank/ControllerObject.java
@@ -0,0 +1,20 @@
+package android.atilim.tr.questionsbank;
+
+import java.util.ArrayList;
+
+/**
+ * Created by efraat on 08.12.2015.
+ */
+public class ControllerObject {
+    private ArrayList<Questions> questions;
+    private String[] answers;
+
+    public ArrayList<Questions> getQuestions() {
+        return questions;
+    }
+
+    public String[] getAnswers() {
+        return answers;
+
+    }
+}
--- app/src/main/java/android/atilim/tr/questionsbank/QuestionView.java
+++ app/src/main/java/android/atilim/tr/questionsbank/QuestionView.java
@@ -1,20 +1,58 @@
 package android.atilim.tr.questionsbank;
 
+import android.atilim.tr.questionsbank.ControllerObject;
+import android.atilim.tr.questionsbank.Questions;
+import android.atilim.tr.questionsbank.R;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class QuestionView extends AppCompatActivity {
+    private  ArrayList<Questions> arrayOfQuestions;
+    private int currentQuestionNumber;
+    private int trueNumberofQuestions;
+    private int falseQuestionNumber;
+
+    public int getCurrentQuestionNumber() {
+        return currentQuestionNumber;
+    }
+
+    public int getTrueNumberofQuestions() {
+        return trueNumberofQuestions;
+    }
+
+    public int getFalseQuestionNumber() {
+        return falseQuestionNumber;
+    }
+
+    private String[] answers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ControllerObject controllerObject=new ControllerObject();
+        arrayOfQuestions=controllerObject.getQuestions();
 
+        TextView questionText=(TextView) findViewById(R.id.question_text);
+        TextView currentQuestionText=(TextView) findViewById(R.id.question_number_text);
+        TextView falseNumberofQuestions=(TextView) findViewById(R.id.false_number_text );
+        TextView trueNumberofQuestions=(TextView) findViewById(R.id.true_number_text );
+        answers = arrayOfQuestions.get(currentQuestionNumber).getAnswers();
+        String questionString=arrayOfQuestions.get(currentQuestionNumber).getQuestionText();
+        currentQuestionNumber=arrayOfQuestions.get(currentQuestionNumber).getIdQuestion();
+
+        questionText.setText(questionString);
+        currentQuestionText.setText(currentQuestionNumber);
+        falseNumberofQuestions.setText(getFalseQuestionNumber());
+        trueNumberofQuestions.setText(getTrueNumberofQuestions());
 
 
     }
